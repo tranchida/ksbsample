28dec45581f106efd60c33adc93626dc2709fe64
Giampaolo Tranchida <giampaolo@tranchida.ch>
2023-12-27 12:16:15 +0100

    fix spring camel

--- src/main/java/ch/tranchida/ksb/MyApplication.java
+++ src/main/java/ch/tranchida/ksb/MyApplication.java
@@ -11,9 +11,7 @@ public final class MyApplication {
     }
 
     public static void main(String[] args) throws Exception {
-        // use Camels Main class
         Main main = new Main();
-        // now keep the application running until the JVM is terminated (ctrl + c or sigterm)
         main.run(args);
     }
 
