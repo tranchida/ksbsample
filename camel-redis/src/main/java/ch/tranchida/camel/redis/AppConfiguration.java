08a8109fc036b756b37645cdb051d98e5e3760af
Giampaolo Tranchida <giampaolo@tranchida.ch>
2024-03-16 06:04:29 +0100

    update

---
+++ camel-redis/src/main/java/ch/tranchida/camel/redis/AppConfiguration.java
@@ -0,0 +1,12 @@
+package ch.tranchida.camel.redis;
+
+import org.apache.camel.BindToRegistry;
+import org.apache.camel.Configuration;
+
+
+@Configuration
+public class AppConfiguration {
+    
+
+
+}
---
+++ camel-redis/src/main/java/ch/tranchida/camel/redis/AppMain.java
@@ -0,0 +1,12 @@
+package ch.tranchida.camel.redis;
+
+import org.apache.camel.main.Main;
+
+public class AppMain {
+    public static void main(String[] args) throws Exception{
+
+        Main main = new Main(AppMain.class);
+        main.run(args);
+
+    }
+}
